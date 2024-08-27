afec3cff07b59a15cbf8b6342f264d02f4892389
Ronny John <me@ronnyjohnti.dev>
2024-08-27 10:43:59 -0300

    feat: Implementando middleware de verificação do token

---
+++ app/Middleware/ApiTokenMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Hyperf\HttpServer\Contract\RequestInterface;
+use Hyperf\HttpServer\Contract\ResponseInterface as HttpResponse;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+use function Hyperf\Support\env;
+
+class ApiTokenMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        protected ContainerInterface $container,
+        protected HttpResponse $response,
+        protected RequestInterface $request
+    ) {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $apiKey = $request->getHeader('Authorization');
+        if (! in_array(env('AUTHORIZATION_API_KEY'), $apiKey)) {
+            return $this->response->withStatus(401);
+        }
+
+        return $handler->handle($request);
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,17 +1,9 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
- */
-
 use App\Controller\AgentQuotasPolicyController;
 use App\Controller\QuotaController;
+use App\Middleware\ApiTokenMiddleware;
 use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
@@ -35,4 +27,4 @@ Router::addGroup('/api/v1', function () {
         Router::addRoute(['POST', 'PUT'], '[/]', [AgentQuotasPolicyController::class, 'store']);
         Router::delete('/{id}[/]', [AgentQuotasPolicyController::class, 'delete']);
     });
-});
+}, ['middleware' => [ApiTokenMiddleware::class]]);
